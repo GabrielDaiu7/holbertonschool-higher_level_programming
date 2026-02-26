3947ea9879b693fd6b7942f3156a3432098d4b40
GabrielDaiu7 <gabrieldaiu909@gmail.com>
2026-02-26 18:45:35 +0100

    update

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- write a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
---use database hbtn_0d_usa
+-- use database hbtn_0d_usa
 USE hbtn_0d_usa;
 
 -- write a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your
