14d5cfb98dba0c46548cde48d4c22b4746a35f6e
GabrielDaiu7 <gabrieldaiu909@gmail.com>
2026-02-26 18:17:58 +0100

    update

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,11 +1,5 @@
--- write a script that print all privileges of user : user_0d_1
--- create users 
-CREATE USER 'user_0d_1'@'localhost';
-CREATE USER 'user_0d_2'@'localhost';
-
--- grant users privileges
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
+-- Print all privileges of user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- write a script that print all privileges of user : user_0d_2
+
+-- Print all privileges of user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
