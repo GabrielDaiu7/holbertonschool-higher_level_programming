e10daf7f3b9f76070b617503e3073473d2f7501c
GabrielDaiu7 <gabrieldaiu909@gmail.com>
2026-02-26 18:42:10 +0100

    update

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
--- Print all privileges of user_0d_1
+-- print all privileges of user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- Print all privileges of user_0d_2
+-- print all privileges of user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
